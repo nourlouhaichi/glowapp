69d91a075bff596b41916bcee5f6a9afd419de72
nourlouhaichi <Nour.Louhaichi@esprit.tn>
2024-02-16 10:49:13 +0100

    Update Admin + CSS

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,7 +17,7 @@ class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $isEditing = $options['is_editing'];
+        // $isEditing = $options['is_editing'];
 
         $builder
             ->add('lastname')
@@ -43,8 +43,8 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
-                // 'mapped' => false,
-                // 'attr' => ['autocomplete' => 'new-password'],
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
